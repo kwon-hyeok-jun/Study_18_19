9b31abe8e187ebf1ffdef84f07d43f2ed0b12216
Kwon <kkynks0@naver.com>
2022-07-20 22:15:24 +0900

    19강 학습

--- Study_18_19/Program.cs
+++ Study_18_19/Program.cs
@@ -85,3 +85,118 @@ class ArrayThree
 
 //Study_19
 using System;
+
+
+/*
+class ReturnValue
+{
+    static string GetString()
+    {
+        return "반환값(Return Value)";
+    }
+
+    static void Main()
+    {
+        string returnValue = GetString();
+        Console.WriteLine(returnValue);
+    }
+}
+*/
+
+/*
+class FunctionReturnValue
+{
+    static int SquareFunction(int x)
+    {
+        return x * x;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(SquareFunction(2));
+    }
+}
+*/
+
+/*
+class FunctionAddNumbers
+{
+    /// <summary>
+    /// 두 수를 더하여 그 결괏값을 반환시켜 주는 함수
+    /// </summary>
+    /// <param name="a">첫 번째 매개변수</param>
+    /// <param name="b">두 번째 매개변수</param>
+    /// <returns>a + b 결과</returns>
+    static int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+
+    static void Main(string[] args)
+    {
+        int a = 3;
+        int b = 5;
+        int c = AddNumbers(3, 5);
+        Console.WriteLine($"{a} + {b} = {c} ");
+    }
+}
+*/
+
+/*
+class NamedParameter
+{
+    static void Main()
+    {
+        Sum(10, 20);
+        Sum(first: 10, second: 20);
+        Sum(second: 10, first: 20);
+
+    }
+
+    static void Sum(int first, int second)
+    {
+        Console.WriteLine(first - second);
+    }
+}
+*/
+
+/*
+class FunctionOverload
+{
+    static void Main()
+    {
+        Multi();
+        Multi("반갑습니다.");
+        Multi("잘지내", 3);
+    }
+
+    static void Multi()
+    {
+        Console.WriteLine("안녕하세요.");
+    }
+
+    static void Multi(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    static void Multi(string message, int count)
+    {
+        for(int i = 0; i < count; i++)
+        {
+            Console.WriteLine("{0}", message);
+        }
+    }
+}
+*/
+
+class CommandLineArgument
+{
+    static void Main(string[] args)
+    {
+        foreach(string arg in args)
+        {
+            Console.WriteLine(arg);
+        }    
+    }
+}
